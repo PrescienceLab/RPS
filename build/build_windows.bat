ca823b5651f5fd11de6db6397ff5666a6bd7e2b2
pdinda <pdinda>
2001-04-19 00:29:42 +0000

    *** empty log message ***

--- build/build_windows.bat
+++ build/build_windows.bat
@@ -1,4 +1,5 @@
 set RPS_DIR=.
+set TYPE=DEBUG
 
 msdev RPS.dsw /MAKE ALL /REBUILD
 
@@ -10,37 +11,19 @@ copy %RPS_DIR%\TimeSeries\include\*.h %RPS_DIR%\include
 copy %RPS_DIR%\RPSInterface\include\*.h %RPS_DIR%\include
 copy %RPS_DIR%\RemosInterface\include\*.h %RPS_DIR%\include
 
-copy %RPS_DIR%\Sensors\GetLoadAvg\Debug\*.exe %RPS_DIR%\Debug
-copy %RPS_DIR%\Sensors\GetLoadAvg\Debug\*.lib %RPS_DIR%\Debug
+copy %RPS_DIR%\Sensors\GetLoadAvg\%TYPE%\*.exe %RPS_DIR%\bin\I386\WIN32
+copy %RPS_DIR%\Sensors\GetLoadAvg\%TYPE%\*.lib %RPS_DIR%\lib\I386\WIN32
 
-copy %RPS_DIR%\Sensors\GetFlowBW\Debug\*.exe %RPS_DIR%\Debug
-copy %RPS_DIR%\Sensors\GetFlowBW\Debug\*.lib %RPS_DIR%\Debug
+copy %RPS_DIR%\Sensors\GetFlowBW\%TYPE%\*.exe %RPS_DIR%\bin\I386\WIN32
+copy %RPS_DIR%\Sensors\GetFlowBW\%TYPE%\*.lib %RPS_DIR%\lib\I386\WIN32
 
-copy %RPS_DIR%\Mirror\Debug\*.lib %RPS_DIR%\Debug
+copy %RPS_DIR%\Mirror\%TYPE%\*.lib %RPS_DIR%\lib\I386\WIN32
 
-copy %RPS_DIR%\TimeSeries\Debug\*.exe %RPS_DIR%\Debug
-copy %RPS_DIR%\TimeSeries\Debug\*.lib %RPS_DIR%\Debug
+copy %RPS_DIR%\TimeSeries\%TYPE%\*.exe %RPS_DIR%\bin\I386\WIN32
+copy %RPS_DIR%\TimeSeries\%TYPE%\*.lib %RPS_DIR%\lib\I386\WIN32
 
-copy %RPS_DIR%\RPSInterface\Debug\*.lib %RPS_DIR%\Debug
+copy %RPS_DIR%\RPSInterface\%TYPE%\*.lib %RPS_DIR%\lib\I386\WIN32
 
-copy %RPS_DIR%\RemosInterface\Debug\*.lib %RPS_DIR%\Debug
+copy %RPS_DIR%\RemosInterface\%TYPE%\*.lib %RPS_DIR%\lib\I386\WIN32
 
-copy %RPS_DIR%\PredComp\Debug\*.exe %RPS_DIR%\Debug
-
-copy %RPS_DIR%\Sensors\GetLoadAvg\Release\*.exe %RPS_DIR%\Release
-copy %RPS_DIR%\Sensors\GetLoadAvg\Release\*.lib %RPS_DIR%\Release
-
-copy %RPS_DIR%\Sensors\GetFlowBW\Release\*.exe %RPS_DIR%\Release
-copy %RPS_DIR%\Sensors\GetFlowBW\Release\*.lib %RPS_DIR%\Release
-
-copy %RPS_DIR%\Mirror\Release\*.lib %RPS_DIR%\Release
-
-copy %RPS_DIR%\TimeSeries\Release\*.exe %RPS_DIR%\Release
-copy %RPS_DIR%\TimeSeries\Release\*.lib %RPS_DIR%\Release
-
-copy %RPS_DIR%\RPSInterface\Release\*.lib %RPS_DIR%\Release
-
-copy %RPS_DIR%\RemosInterface\Release\*.lib %RPS_DIR%\Release
-
-copy %RPS_DIR%\PredComp\Release\*.exe %RPS_DIR%\Release
-
+copy %RPS_DIR%\PredComp\%TYPE%\*.exe %RPS_DIR%\bin\I386\WIN32
