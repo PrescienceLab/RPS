981beb2bf701ef5b4a3dddac81ad99b54e1a5c16
pdinda <pdinda>
2002-12-16 00:23:53 +0000

    *** empty log message ***

--- Web/oneoffpred.pl
+++ Web/oneoffpred.pl
@@ -1,8 +1,42 @@
 #!/usr/bin/perl -w
 
 
-@input=<STDIN>;
-$input=join("\n",@input);
+use CGI;
+use POSIX;
 
-print "$input\n";
+$ENV{"RPS_DIR"} or die "set RPS_DIR\n";
 
+$unique=getpid().rand();
+$unique=~s/\s+//g;
+
+$dir=$ENV{"RPS_DIR"}."/Web/Temp";
+
+$infile="$dir/$unique.in";
+$outfile="$dir/$unique.out";
+$giffile="$dir/$unique.gif";
+
+$query=new CGI;
+
+$filename=$query->param("inputfile");
+$fitfirst=$query->param("fitfirst");
+$fitnum=$query->param("fitnum");
+$testnum=$query->param("testnum");
+$numahead=$query->param("numahead");
+$model=$query->param("model");
+
+$fh = $query->upload("inputfile");
+open(UPLOAD,">$infile");
+binmode(UPLOAD);
+while (<$fh>) { print UPLOAD; }
+close(UPLOAD);
+
+print "Filename: $filename\n";
+print "Fitfirst: $fitfirst\n";
+print "Fitnum:   $fitnum\n";
+print "Testnum:  $testnum\n";
+print "Numahead: $numahead\n";
+print "Model:    $model\n";
+
+open(INPUT,"$infile");
+while (<INPUT>) { print;}
+close(INPUT);
