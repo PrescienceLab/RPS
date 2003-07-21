69d1ba3a5243887785fec6b71b5693984d2993fc
jskitz <jskitz>
2003-07-21 00:33:54 +0000

    First static mixed file and the makefile checked in.

--- Wavelets/src/sample_static_mixed_sfwt.cpp
+++ Wavelets/src/sample_static_mixed_sfwt.cpp
@@ -3,6 +3,7 @@
 #include <fstream>
 #include <vector>
 #include <cmath>
+#include <limits.h>
 
 #include "banner.h"
 #include "waveletsample.h"
@@ -15,32 +16,32 @@ void usage()
   char *tb=GetTsunamiBanner();
   char *b=GetRPSBanner();
 
-  cerr << " sample_static_sfwt [input-file] [wavelet-type-init]\n";
-  cerr << "  [numstages-init] [transform-type] [output-file] [flat]\n\n";
+  cerr << " sample_static_mixed_sfwt [input-file] [wavelet-type-init]\n";
+  cerr << "  [numstages-init] [output-file] [specification-file]\n";
+  cerr << "  [flat]\n\n";
   cerr << "--------------------------------------------------------------\n";
   cerr << "\n";
-  cerr << "[input-file]        = The name of the file containing time-\n";
-  cerr << "                      domain samples.  Can also be stdin.\n";
+  cerr << "[input-file]         = The name of the file containing time-\n";
+  cerr << "                       domain samples.  Can also be stdin.\n";
   cerr << "\n";
-  cerr << "[wavelet-type-init] = The type of wavelet.  The choices are\n";
-  cerr << "                      {DAUB2 (Haar), DAUB4, DAUB6, DAUB8,\n";
-  cerr << "                      DAUB10, DAUB12, DAUB14, DAUB16, DAUB18,\n";
-  cerr << "                      DAUB20}.  The 'DAUB' stands for\n";
-  cerr << "                      Daubechies wavelet types and the order\n";
-  cerr << "                      is the number of coefficients.\n";
+  cerr << "[wavelet-type-init]  = The type of wavelet.  The choices are\n";
+  cerr << "                       {DAUB2 (Haar), DAUB4, DAUB6, DAUB8,\n";
+  cerr << "                       DAUB10, DAUB12, DAUB14, DAUB16, DAUB18,\n";
+  cerr << "                       DAUB20}.  The 'DAUB' stands for\n";
+  cerr << "                       Daubechies wavelet types and the order\n";
+  cerr << "                       is the number of coefficients.\n";
   cerr << "\n";
-  cerr << "[numstages-init]    = The number of stages to use in the\n";
-  cerr << "                      decomposition.  The number of levels is\n";
-  cerr << "                      equal to the number of stages + 1.\n";
+  cerr << "[numstages-init]     = The number of stages to use in the\n";
+  cerr << "                       decomposition.  The number of levels is\n";
+  cerr << "                       equal to the number of stages + 1.\n";
   cerr << "\n";
-  cerr << "[transform-type]    = The transform type may be of type\n";
-  cerr << "                      APPROX | DETAIL | TRANSFORM.\n";
+  cerr << "[output-file]        = Which file to write the output.  This may\n";
+  cerr << "                       also be stdout or stderr.\n\n";
   cerr << "\n";
-  cerr << "[output-file]       = Which file to write the output.  This may\n";
-  cerr << "                      also be stdout or stderr.\n\n";
+  cerr << "[specification-file] = Mixed signal specification.\n";
   cerr << "\n";
-  cerr << "[flat]              = Whether the output is flat or human\n";
-  cerr << "                      readable.  flat | noflat to choose.\n";
+  cerr << "[flat]               = Whether the output is flat or human\n";
+  cerr << "                       readable.  flat | noflat to choose.\n";
   cerr << "\n";
   cerr << tb << endl;
   cerr << b << endl;
@@ -76,6 +77,42 @@ WaveletType GetWaveletType(const char *x, const char *filename)
    }
 }
 
+void ParseSignalSpec(SignalSpec &spec, ifstream &file)
+{
+  const char pound = '#';
+  const char approx = 'A';
+  const char detail = 'D';
+  const char space = ' ';
+  char c;
+
+  unsigned i;
+  unsigned numlevels;
+  int levelnum;
+
+  while ( (c=file.get()) != EOF) {
+    if (c == pound) {
+      file.ignore(SHRT_MAX, '\n');
+    } else if (c == approx) {
+      file.ignore(SHRT_MAX, space);
+      file >> numlevels;
+      for (i=0; i<numlevels; i++) {
+	file >> levelnum;
+	spec.approximations.push_back(levelnum);
+      }
+    } else if (c == detail) {
+      file.ignore(SHRT_MAX, space);
+      file >> numlevels;
+      for (i=0; i<numlevels; i++) {
+	file >> levelnum;
+	spec.details.push_back(levelnum);
+      }
+    } else if (c == space) {
+      file.ignore(SHRT_MAX, '\n');
+    }
+  }
+}
+
+
 int main(int argc, char *argv[])
 {
   if (argc!=7) {
@@ -88,7 +125,7 @@ int main(int argc, char *argv[])
   } else {
     infile.open(argv[1]);
     if (!infile) {
-      cerr << "sample_static_sfwt: Cannot open input file " << argv[1] << ".\n";
+      cerr << "sample_static_mixed_sfwt: Cannot open input file " << argv[1] << ".\n";
       exit(-1);
     }
     cin = infile;
@@ -98,46 +135,44 @@ int main(int argc, char *argv[])
 
   int numstages = atoi(argv[3]);
   if (numstages <= 0) {
-    cerr << "sample_static_sfwt: Number of stages must be positive.\n";
+    cerr << "sample_static_mixed_sfwt: Number of stages must be positive.\n";
     exit(-1);
   }
   unsigned numlevels = numstages + 1;
 
-  TransformType tt;
-  if (toupper(argv[4][0])=='A') {
-    tt = APPROX;
-  } else if (toupper(argv[4][0])=='D') {
-    tt = DETAIL;
-  } else if (toupper(argv[4][0])=='T') {
-    tt = TRANSFORM;
-  } else {
-    cerr << "sample_static_sfwt: Invalid transform type.  Choose APPROX | DETAIL | TRANSFORM.\n";
-    usage();
-    exit(-1);
-  }
-
   ostream outstr;
   ofstream outfile;
-  if (!strcasecmp(argv[5],"stdout")) {
+  if (!strcasecmp(argv[4],"stdout")) {
     outstr.tie(&cout);
-  } else if (!strcasecmp(argv[5],"stderr")) {
+  } else if (!strcasecmp(argv[4],"stderr")) {
     outstr.tie(&cerr);
   } else {
-    outfile.open(argv[5]);
+    outfile.open(argv[4]);
     if (!outfile) {
-      cerr << "sample_static_sfwt: Cannot open output file " << argv[5] << ".\n";
+      cerr << "sample_static_mixed_sfwt: Cannot open output file " << argv[5] << ".\n";
       exit(-1);
     }
     outstr.tie(&outfile);
+  }
+
+  ifstream specfile;
+  specfile.open(argv[5]);
+  if (!specfile) {
+    cerr << "sample_static_mixed_sfwt: Cannot open specification file " << argv[5] << ".\n";
+    exit(-1);
   }
 
   bool flat=true;
   if (toupper(argv[6][0])=='N') {
     flat = false;
   } else if (toupper(argv[6][0])!='F') {
-    cerr << "sample_static_sfwt: Need to choose flat or noflat for human readable.\n";
+    cerr << "sample_static_mixed_sfwt: Need to choose flat or noflat for human readable.\n";
     exit(-1);
   }
+
+  SignalSpec sigspec;
+  ParseSignalSpec(sigspec, specfile);
+  specfile.close();
 
   unsigned i;
   typedef WaveletInputSample<double> wisd;
@@ -159,99 +194,61 @@ int main(int argc, char *argv[])
   StaticForwardWaveletTransform<double, wosd, wisd> sfwt(numstages,wt,2,2,0);
 
   // Create result buffers
-  vector<wosd> outsamples;
+  vector<wosd> approxout;
+  vector<wosd> detailout;
 
   // Create vectors for the level outputs
-  vector<deque<wosd> *> levels;
+  vector<deque<wosd> *> approxlevels;
+  vector<deque<wosd> *> detaillevels;
+  deque<wosd>* pwos;
   for (i=0; i<numlevels; i++) {
-    deque<wosd>* pwos = new deque<wosd>();
-    levels.push_back(pwos);
+    pwos = new deque<wosd>();
+    approxlevels.push_back(pwos);
+
+    pwos = new deque<wosd>();
+    detaillevels.push_back(pwos);
   }
 
-  switch(tt) {
-  case APPROX: {
-    for (i=0; i<samples.size(); i++) {
-      sfwt.StreamingApproxSampleOperation(outsamples, samples[i]);
+  for (i=0; i<samples.size(); i++) {
+    sfwt.StreamingMixedSampleOperation(approxout, detailout, samples[i], sigspec);
 
-      if (flat) {
-	*outstr.tie() << i << "\t" << outsamples.size() << "\t";
-      }
-
-      for (unsigned j=0; j<outsamples.size(); j++) {
-	int samplelevel = outsamples[j].GetSampleLevel();
-	levels[samplelevel]->push_front(outsamples[j]);
-	if (flat) {
-	  *outstr.tie() << samplelevel << " " << outsamples[j].GetSampleValue() << "\t";
-	}
-      }
-      if (flat) {
-	*outstr.tie() << endl;
-      }
-
-      outsamples.clear();
+    if (flat) {
+      *outstr.tie() << i << "\t" << "A\t" << approxout.size() << "\t";
     }
-    numlevels -= 1;
-  }
-  break;
 
-  case DETAIL: {
-    for (i=0; i<samples.size(); i++) {
-      sfwt.StreamingDetailSampleOperation(outsamples, samples[i]);
-
+    for (unsigned j=0; j<approxout.size(); j++) {
+      int samplelevel = approxout[j].GetSampleLevel();
+      approxlevels[samplelevel]->push_front(approxout[j]);
       if (flat) {
-	*outstr.tie() << i << "\t" << outsamples.size() << "\t";
+	*outstr.tie() << samplelevel << " " << approxout[j].GetSampleValue() << "\t";
       }
-
-      for (unsigned j=0; j<outsamples.size(); j++) {
-	int samplelevel = outsamples[j].GetSampleLevel();
-	levels[samplelevel]->push_front(outsamples[j]);
-	if (flat) {
-	  *outstr.tie() << samplelevel << " " << outsamples[j].GetSampleValue() << "\t";
-	}
-      }
-      if (flat) {
-	*outstr.tie() << endl;
-      }
-
-      outsamples.clear();
     }
-    numlevels -= 1;
-  }
-  break;
 
-  case TRANSFORM: {
-    for (i=0; i<samples.size(); i++) {
-      sfwt.StreamingTransformSampleOperation(outsamples, samples[i]);
-
-      if (flat) {
-	*outstr.tie() << i << "\t" << outsamples.size() << "\t";
-      }
-
-      for (unsigned j=0; j<outsamples.size(); j++) {
-	int samplelevel = outsamples[j].GetSampleLevel();
-	levels[samplelevel]->push_front(outsamples[j]);
-	if (flat) {
-	  *outstr.tie() << samplelevel << " " << outsamples[j].GetSampleValue() << "\t";
-	}
-      }
-      if (flat) {
-	*outstr.tie() << endl;
-      }
-
-      outsamples.clear();
+    if (flat) {
+      *outstr.tie() << endl << i << "\t" << "D\t" << detailout.size() << "\t";
     }
-  }
-  break;
 
-  default:
-    break;
+    for (unsigned j=0; j<detailout.size(); j++) {
+      int samplelevel = detailout[j].GetSampleLevel();
+      detaillevels[samplelevel]->push_front(detailout[j]);
+      if (flat) {
+	*outstr.tie() << samplelevel << " " << detailout[j].GetSampleValue() << "\t";
+      }
+    }
+    if (flat) {
+      *outstr.tie() << endl;
+    }
+
+    detailout.clear();
+    approxout.clear();
   }
+  numlevels -= 1;
 
   // Human readable output
   if (!flat) {
-    *outstr.tie() << "The size of each level:" << endl;
+    *outstr.tie() << "The size of each approximation level:" << endl;
     for (i=0; i<numlevels; i++) {
-      *outstr.tie() << "\tLevel " << i << " size = " << levels[i]->size() << endl;
+      *outstr.tie() << "\tLevel " << i << " size = " << approxlevels[i]->size() << endl;
     }
     *outstr.tie() << endl;
 
@@ -265,16 +262,59 @@ int main(int argc, char *argv[])
     }
     *outstr.tie() << endl;
 
-    unsigned loopsize = levels[0]->size();
+    unsigned loopsize=0;
+    for (i=0; i<numlevels; i++) {
+      if (approxlevels[i]->size() > loopsize) {
+	loopsize = approxlevels[i]->size();
+      }
+    }
+
     for (i=0; i<loopsize; i++) {
       *outstr.tie() << i << "\t";
 
       for (unsigned j=0; j<numlevels; j++) {
-	if (!levels[j]->empty()) {
+	if (!approxlevels[j]->empty()) {
 	  wosd wos;
-	  wos = levels[j]->back();
+	  wos = approxlevels[j]->back();
 	  *outstr.tie() << wos.GetSampleValue() << "\t";
-	  levels[j]->pop_back();
+	  approxlevels[j]->pop_back();
+	}
+      }
+      *outstr.tie() << endl;
+    }
+
+    *outstr.tie() << "The size of each detail level:" << endl;
+    for (i=0; i<numlevels; i++) {
+      *outstr.tie() << "\tLevel " << i << " size = " << detaillevels[i]->size() << endl;
+    }
+    *outstr.tie() << endl;
+
+    *outstr.tie() << "Index     ";
+    for (i=0; i<numlevels; i++) {
+      *outstr.tie() << "Level " << i << "        " ;
+    }
+    *outstr.tie() << endl << "-----     ";
+    for (i=0; i<numlevels; i++) {
+      *outstr.tie() << "-------        ";
+    }
+    *outstr.tie() << endl;
+
+    loopsize=0;
+    for (i=0; i<numlevels; i++) {
+      if (detaillevels[i]->size() > loopsize) {
+	loopsize = detaillevels[i]->size();
+      }
+    }
+
+    for (i=0; i<loopsize; i++) {
+      *outstr.tie() << i << "\t";
+
+      for (unsigned j=0; j<numlevels; j++) {
+	if (!detaillevels[j]->empty()) {
+	  wosd wos;
+	  wos = detaillevels[j]->back();
+	  *outstr.tie() << wos.GetSampleValue() << "\t";
+	  detaillevels[j]->pop_back();
 	}
       }
       *outstr.tie() << endl;
@@ -282,9 +322,11 @@ int main(int argc, char *argv[])
   }
   
   for (i=0; i<numlevels; i++) {
-    CHK_DEL(levels[i]);
+    CHK_DEL(approxlevels[i]);
+    CHK_DEL(detaillevels[i]);
   }
-  levels.clear();
+  approxlevels.clear();
+  detaillevels.clear();
 
   return 0;
 }
